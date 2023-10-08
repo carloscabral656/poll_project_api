94c7e9eb26724af479b9938ceaa139cae8228e1d
Carlos Eduardo <edu.carlos.cec@gmail.com>
2023-10-07 21:49:16 -0300

    Recreating order for migrations

---
+++ database/migrations/2023_10_07_000006_create_answers_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('answers', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('id_question');
+            $table->foreign('id_question')->references('id')->on('questions');
+            $table->unsignedBigInteger('id_user');
+            $table->foreign('id_user')->references('id')->on('users');
+            $table->unsignedBigInteger('id_avaliation');
+            $table->foreign('id_avaliation')->references('id')->on('avaliations');
+            $table->dateTime('date_answer');
+            $table->string('comment')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('answer');
+    }
+};
