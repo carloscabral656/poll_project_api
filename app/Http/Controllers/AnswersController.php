eadca6f3c412dc6a8f20e36f992a0c3a98924226
Carlos Eduardo <edu.carlos.cec@gmail.com>
2023-10-28 15:36:52 -0300

    Answers Controller, index method

---
+++ app/Http/Controllers/AnswersController.php
@@ -0,0 +1,81 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\DTO\ApiResponse;
+use App\Services\AnswersService;
+use Illuminate\Http\Request;
+
+class AnswersController extends Controller
+{
+
+    protected AnswersService $answersService;
+    protected ApiResponse $apiResponse;
+
+    public function __construct()
+    {
+        $this->answersService = app(AnswersService::class);
+        $this->apiResponse  = app(ApiResponse::class);
+    }
+
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $answers = $this->answersService->getAll();
+        return $this->apiResponse
+                    ->setSuccess(false)
+                    ->setContent($answers)
+                    ->setStatusCode(StatusCode::OK)
+                    ->create();
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+    }
+}
