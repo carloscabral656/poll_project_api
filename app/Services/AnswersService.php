b6d68de95ff8d8fb079c291de170a57b91ba875a
Carlos Eduardo <edu.carlos.cec@gmail.com>
2023-11-15 09:08:12 -0300

    Creating answers. Users entitie need to be created.

--- app/Services/AnswersService.php
+++ app/Services/AnswersService.php
@@ -4,6 +4,7 @@ namespace App\Services;
 
 use App\Models\Answer;
 use App\Models\Question;
+use DateTime;
 use Exception;
 use Illuminate\Database\QueryException;
 use Illuminate\Support\Facades\DB;
@@ -34,10 +35,25 @@ class AnswersService extends ServiceAbstract
     */
     public function store($data){
         try{
+
+            //return (new DateTime());
+
             DB::beginTransaction();
-            $answer = $data;
-            unset($answer['answers']);
-            $answers = $this->getModel()->create($answer);  
+            
+            // Insert process
+            $answers = [];
+            foreach($data['answers'] as $answer){
+                $answer = [
+                    'id_user'       => 1, //TODO:Create user after this
+                    'id_avaliation' => $answer,
+                    'date_answer'   => $data['dateAnswer'],
+                    'comment'       => $data['comment']
+                ];
+                $answers[] = $this->getModel()->create($answer); 
+            }
+
+            // 
+            return $answers;
             
             // Finding question to sync with answer
             $question = $this->questionsService->get((int) $data['idQuestion']);
